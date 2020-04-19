a6f0c1d7ae2123e2284763db9e5d72fe023688ac
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-04-19 14:44:06 -0300

    Creates DTO and named params

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -24,7 +24,10 @@ appointmentsRouter.post('/', (request, response) => {
       .status(400)
       .json({ message: 'This appointment is already booked' });
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
