ef91b6ae2edc68a7174d094e95fc488fbec710dd
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-04-22 08:36:41 -0300

    Changes provider to provider_id to add relation between users and appointments

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,12 +4,12 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -22,7 +22,7 @@ class CreateAppointmentService {
       throw Error('This appointment is already booked');
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
