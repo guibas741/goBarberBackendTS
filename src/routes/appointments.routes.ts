0fa470d6722ec3db3a6c4aac6233024aa39af0bf
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-04-22 21:21:52 -0300

    Creates middleware to validate token

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,10 +3,14 @@ import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
+appointmentsRouter.use(ensureAuthenticated);
+
 appointmentsRouter.get('/', async (request, response) => {
+  console.log(request.user);
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
