77ba8ebdf7a42795bec6c1e7603c843c9ea0c641
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-05 08:05:19 -0300

    Separates application on modules

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
-import routes from './routes';
+import routes from './shared/routes';
 import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import AppError from './shared/errors/AppError';
 
-import './database';
+import './shared/database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
