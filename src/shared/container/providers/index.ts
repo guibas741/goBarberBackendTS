e44db41721cfe197121f5cd89fab744ca2b1d4e5
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-10 14:00:07 -0300

    Configs nodemailler with ethereal to send emails on develop mode

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,9 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailprovider from './MailProvider/models/IMailProvider';
+import IMailprovider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailprovider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
