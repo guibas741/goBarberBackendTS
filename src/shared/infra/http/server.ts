8b03d855c2ebd7a21788ca04b38ec9af4b6681bc
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-15 08:17:49 -0300

    Creates middleware to limit users requests by seconds using node-rate-limiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,8 +5,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
+
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/RateLimiter';
 import routes from './routes';
 
 import '../typeorm';
@@ -14,6 +16,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
